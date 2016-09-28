4d6b5d0c85536a9bcc9bd354709f771e726536dc
Jakub Stefanski <iakub.stefanski+git@gmail.com>
2016-09-29 00:31:52 +0200

    Add cowboy handler behavior and docs

--- src/rabbit_wh_http_health.erl
+++ src/rabbit_wh_http_health.erl
@@ -6,8 +6,9 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(rabbit_wh_http_health).
+-behavior(cowboy_http_handler).
 
-%% Cowboy handler callbacks
+%% Cowboy HTTP handler callbacks
 -export([init/3,
          handle/2,
          terminate/3]).
@@ -19,7 +20,19 @@
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Initializes the handler
+%% Initializes the request handler
+%%
+%% @spec init(Type, Req, State) -> {ok, Req, State} |
+%%                                 {loop, Req, State} |
+%%                                 {loop, Req, State, hibernate} |
+%%                                 {loop, Req, State, timeout()} |
+%%                                 {loop, Req, State, timeout(), hibernate} |
+%%                                 {shutdown, Req, State} |
+%%                                 {upgrade, protocol, module()} |
+%%                                 {upgrade, protocol, module(), Req, any()}
+%%      Type = {atom(), http}
+%%      Req = cowboy_req:req()
+%%      State = any()
 %% @end
 %%--------------------------------------------------------------------
 init(_Type, Req, []) ->
@@ -29,6 +42,10 @@ init(_Type, Req, []) ->
 %% @private
 %% @doc
 %% Handles the request
+%%
+%% @spec handle(Req, State) -> {ok, Req, State}
+%%      Req = cowboy_req:req()
+%%      State = any()
 %% @end
 %%--------------------------------------------------------------------
 handle(Req, State) ->
@@ -43,6 +60,15 @@ handle(Req, State) ->
 %% @private
 %% @doc
 %% Clean up
+%%
+%% @spec terminate(Reason, Req, State) -> ok
+%%      Reason = {normal, shutdown} |
+%%               {normal, timeout} | %% Only occurs in loop handlers.
+%%               {error, closed} | %% Only occurs in loop handlers.
+%%               {error, overflow} | %% Only occurs in loop handlers.
+%%               {error, atom()}
+%%      Req = cowboy_req:req()
+%%      State = any()
 %% @end
 %%--------------------------------------------------------------------
 terminate(_Reason, _Req, _State) ->
